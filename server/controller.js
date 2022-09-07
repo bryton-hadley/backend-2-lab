e083382a94756fc89e891235880acce7870c3c47
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-07 17:14:25 -0600

    try to fix button

---
+++ server/controller.js
@@ -0,0 +1,44 @@
+const houses = require('./db.json')
+let globelID = 4;
+
+module.exports = {
+    getHouse: (req, res) =>{
+     res.status (200).send(houses)
+    },
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex((element) => element.id === +req.params.id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+    },
+    createHouse: (req, res) => {
+        const {address, price, imageURL,} = req.body;
+        let newHouse = {
+            address,
+            price: +price,
+            imageURL,
+            id: globelID
+        }
+        houses.push(newHouse)
+        res.status(200).send(houses)
+        globelID++;
+    },
+    updateHouse: (req, res) => {
+        let {id} = req.params
+        let {type} = req.body
+
+        let index = houses.findIndex((element) => element.id === +id) 
+
+        if(houses[index].price <= 10000 && type === 'minus'){
+            houses[index].price = 0
+            res.status(200).send(houses)
+        }else if( type === 'plus') {
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        }else if(type === 'minus'){
+            houses[index].price -= 10000
+        }else {
+            res.sendStatus(400)
+        }
+        
+    } 
+}
---
+++ server/index.js
@@ -0,0 +1,34 @@
+// calling variables to set up my boiler plate code for my index.js
+
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+// creating a destuctering fuction
+const {getHouse, 
+    deleteHouse, 
+    createHouse,
+    updateHouse,
+
+} = require('./controller')
+
+//declaring end points for the functions
+app.get('/api/houses', getHouse)
+
+//creating a deleteHouse request 
+
+app.delete('/api/houses/:id', deleteHouse)
+
+//creating a post request
+app.post('/api/houses', createHouse)
+
+// creating a put to updateHoouse 
+app.put('/api/houses/:id', updateHouse)
+
+
+app.listen(4004, () => {console.log('Listen on port 4004')})
